7e497b9f29b43204fc90e07fc77c53e993bd61d1
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-04-09 21:37:46 +0530

    Update MusicPlayerGUI.java

--- mp3/src/MusicPlayerGUI.java
+++ mp3/src/MusicPlayerGUI.java
@@ -1,5 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.*;
 public class MusicPlayerGUI extends JFrame {
+
+   public static final Color FRAME_COLOR = Color.BLACK;
+   public static final Color TEXT_COLOR  = Color.WHITE;
+
+
          public MusicPlayerGUI(){
             super("Music Player");
 
@@ -10,6 +19,8 @@ public class MusicPlayerGUI extends JFrame {
             setResizable(false);
             setLayout(null);
 
+            //change frame color
+            getContentPane().setBackground(FRAME_COLOR);
             addGuiComponents();
          }   
          
@@ -17,8 +28,39 @@ public class MusicPlayerGUI extends JFrame {
             //add toolbar
             addToolbar();
 
-        }
+            //load record image
+            JLabel songImage = new JLabel(loadImage("C:\\Users\\JAHNAVI SAXENA\\OneDrive\\Desktop\\Academic\\SEM 4\\OOPS\\Capstone Project\\M\\mp3\\src\\Assets\\record.png"));
+            songImage.setBounds(0,50, getWidth() - 20, 225);
+            add(songImage);
+
+            //songtitle
+            JLabel songTitle = new JLabel("Song Title");
+            songTitle.setBounds(0,285, getWidth()-10, 30);
+            songTitle.setFont(new Font("Dialog", Font.BOLD, 24));
+            songTitle.setForeground(TEXT_COLOR);
+            songTitle.setHorizontalAlignment(SwingConstants.CENTER);
+            add(songTitle);
+
+            //songartist
+            JLabel songArtist = new JLabel("Artist");
+            songArtist.setBounds(0,315,getWidth()-10,30);
+            songArtist.setFont(new Font("Dialog",Font.PLAIN,24));
+            songArtist.setForeground(TEXT_COLOR);
+            songArtist.setHorizontalAlignment(SwingConstants.CENTER);
+            add(songArtist);
+
+            //playback slider
+            JSlider playbackSlider = new JSlider(JSlider.HORIZONTAL,0,100,0);
+            playbackSlider.setBounds(getWidth()/2-300/2,365,300,40) ;
+            playbackSlider.setBackground(null);
+            add(playbackSlider);
+
+            //playback buttons
+            addPLayBackButtons();
+        }   
+        
          private void addToolbar(){
+
             JToolBar toolbar = new JToolBar();
             toolbar.setBounds(0,0,getWidth(),20); 
             
@@ -48,5 +90,54 @@ public class MusicPlayerGUI extends JFrame {
             playListMenu.add(loadPlaylist);
             add(toolbar);
         }
+
+
+
+        private void addPLayBackButtons(){
+         JPanel playbackButtons = new JPanel();
+         playbackButtons.setBounds(0,435,getWidth()-10,80);
+         playbackButtons.setBackground(null);
+
+         //previous button
+         JButton prevButton = new JButton(loadImage("C:\\Users\\JAHNAVI SAXENA\\OneDrive\\Desktop\\Academic\\SEM 4\\OOPS\\Capstone Project\\M\\mp3\\src\\Assets\\previous.png"));
+         prevButton.setBorderPainted(false);
+         prevButton.setBackground(null);
+         playbackButtons.add(prevButton);
+
+         //play button
+         JButton playButton = new JButton(loadImage("C:\\Users\\JAHNAVI SAXENA\\OneDrive\\Desktop\\Academic\\SEM 4\\OOPS\\Capstone Project\\M\\mp3\\src\\Assets\\play.png"));
+         playButton.setBorderPainted(false);
+         playButton.setBackground(null);
+         playbackButtons.add(playButton);
+
+         //pause button
+         JButton pausebutton = new JButton(loadImage("C:\\Users\\JAHNAVI SAXENA\\OneDrive\\Desktop\\Academic\\SEM 4\\OOPS\\Capstone Project\\M\\mp3\\src\\Assets\\pause.png"));
+         pausebutton.setBorderPainted(false);
+         pausebutton.setBackground(null);
+         pausebutton.setVisible(false);
+         playbackButtons.add(pausebutton);
+
+         //next button
+         JButton nextButton = new JButton(loadImage("C:\\Users\\JAHNAVI SAXENA\\OneDrive\\Desktop\\Academic\\SEM 4\\OOPS\\Capstone Project\\M\\mp3\\src\\Assets\\next.png"));
+         nextButton.setBorderPainted(false);
+         nextButton.setBackground(null);
+         playbackButtons.add(nextButton);
+
+         add(playbackButtons);
+        }
+        private ImageIcon loadImage(String path){
+         try{
+            //read image file from the path
+            BufferedImage image  = ImageIO.read(new File(path));
+
+            return new ImageIcon(image);
+         }catch(Exception e){
+            e.printStackTrace();
+         }
+
+         return null;
+        }
     }
+
+
 
